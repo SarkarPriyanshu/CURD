67026bc386b5ab92313c899d7a2a6509a48dfffd
SarkarPriyanshu <provashpriyanshu@gmail.com>
2021-02-03 02:51:36 +0530

    Complete CURD API

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ dotenv.config()
 const ENV = {
     PORT:process.env.PORT,
     NODE_ENV:process.env.NODE_ENV,
-    DB_URL:"mongodb+srv://test123:test123@tmcuster.spyrr.mongodb.net/Memes?retryWrites=true&w=majority"
+    DB_URL://db link goes here//
 }
 
 module.exports = ENV
